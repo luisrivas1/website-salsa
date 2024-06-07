2899330ab587f0fef935cf7abc2f8bb818cac775
luisrivas1 <rivasbejaranoluis@gmial.com>
2024-06-07 23:42:49 +0200

    base

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './node_modules/flowbite/**/*.js'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#802e2f',
+        'primary-border': '#98504d',
+        secundary: '#0099db',
+        success: '#2e807f',
+        'success-hover': '#296a69',
+        'success-ring': '#9abebd',
+        cancel: '#db4200',
+        info: '#00ffff',
+        danger: '#ff6624',
+        'background-dark': '#000000'
+      }
+    }
+  }
 }
